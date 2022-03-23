185964baa5fa21f607f656f475617e9a16f927c9
Brian Hall <brian.hall4@gmail.com>
2022-03-22 20:47:59 -0400

    let's make this a microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+    },
+  );
+  app.listen(3033);
 }
 bootstrap();
